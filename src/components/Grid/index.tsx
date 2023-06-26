a445be6915c176168e544b2c1c030e92ae5201a2
jcandeli <jpcandelier@gmail.com>
2023-06-25 19:20:48 -0700

    fixed mobile grid

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -20,22 +20,26 @@ interface GridItemProps {
 }
 
 const GridItem: StyledComponent<"div", any, GridItemProps> = styled.div`
-  grid-column: span
-    ${({ orientation }) =>
-      (orientation === "horizontal" && 2) ||
-      (orientation === "block" && 2) ||
-      (orientation === "banner" && 3) ||
-      1};
-  grid-row: span
-    ${({ orientation }) =>
-      (orientation === "vertical" && 2) || (orientation === "block" && 2) || 1};
-
-  /* Set item to fill the entire grid cell */
+  grid-column: span 1;
+  grid-row: span 1;
   width: 100%;
-  height: ${({ orientation }) =>
-    (orientation === "vertical" && "auto") ||
-    (orientation === "block" && "calc((100vw / 2) - 2rem)") ||
-    "calc((100vw / 3) - 2rem)"};
+
+  @media (min-width: 768px) {
+    grid-column: span
+      ${({ orientation }) =>
+        (orientation === "horizontal" && 2) ||
+        (orientation === "block" && 2) ||
+        (orientation === "banner" && 3)};
+    grid-row: span
+      ${({ orientation }) =>
+        (orientation === "vertical" && 2) || (orientation === "block" && 2)};
+
+    /* Set item to fill the entire grid cell */
+    height: ${({ orientation }) =>
+      (orientation === "vertical" && "auto") ||
+      (orientation === "block" && "calc((100vw / 2) - 2rem)") ||
+      "calc((min(100vw, 1400px) / 3) - 2rem)"};
+  }
 `;
 
 /* Export the new component */
